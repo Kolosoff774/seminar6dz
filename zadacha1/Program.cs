aabf0913e222eed33f94e979ffcaa02459735f89
Andrey <akolosov48@gmail.com>
2023-09-09 16:45:32 +0500

    Добавлено дз

---
+++ zadacha1/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+int[] PromptArray(int[] arr) 
+{
+    int i = 0;
+    while (i < arr.Length) 
+    {
+        arr[i] = int.Parse (Console.ReadLine());
+        i++;
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr) 
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int PositiveNumbers(int[] arr) 
+{
+    int num = 0;
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        if (arr[i] > 0) 
+        {
+            num++;
+        }
+    }
+    return num;
+}
+
+Console.WriteLine("Введи количество элементов: ");
+int length = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Введи элементы:");
+int[] array = new int[length];
+PromptArray(array);
+
+PrintArray(array);
+Console.Write($"Количество чисел больше 0 = {PositiveNumbers(array)}");
---
+++ zadacha2/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+double PointIntersectionX(double a1, double a2, int b1, int b2) 
+{
+    double x = (b2 - b1) / (a1 - a2);
+    return Math.Round(x, 2);
+}
+
+double PointIntersectionY(double a1, double a2, int b1, int b2, double x) 
+{
+    double y = a1 * x + b1;
+    return Math.Round(y, 2);
+}
+
+Console.Write("b1 = ");
+int b1 = int.Parse(Console.ReadLine());
+Console.Write("k1 = ");
+double k1 = int.Parse(Console.ReadLine());
+Console.Write("b2 = ");
+int b2 = int.Parse(Console.ReadLine());
+Console.Write("k2 = ");
+double k2 = int.Parse(Console.ReadLine());
+
+
+double resultX = PointIntersectionX(k1, k2, b1, b2);
+double resultY = PointIntersectionY(k1, k2, b1, b2, resultX);
+
+if (k1 == k2) Console.WriteLine("Паралелльные прямые");
+else 
+{
+    Console.WriteLine("Точка пересечений:");
+    Console.WriteLine($"X = {resultX}");
+    Console.WriteLine($"Y = {resultY}");
+}
